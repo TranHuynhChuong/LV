43cf4b358a8fd3188d214c34c27769852ec46214
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-04 21:33:55 +0700

    Refactoring : Delete tabs, convert to dynamic route

--- FrontEnd/Staff/src/app/(main)/products/page.tsx
+++ FrontEnd/Staff/src/app/(main)/products/page.tsx
@@ -1,30 +1,14 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useBreadcrumb } from '@/contexts/BreadcrumbContext';
-import { Tabs, TabsTrigger, TabsList, TabsContent } from '@/components/ui/tabs';
-import ProductLive from './components/productLive';
+import { useRouter } from 'next/navigation';
 
 export default function Products() {
-  const { setBreadcrumbs } = useBreadcrumb();
+  const router = useRouter();
 
   useEffect(() => {
-    setBreadcrumbs([{ label: 'Trang chủ', href: '/' }, { label: 'Sản phẩm' }]);
-  }, [setBreadcrumbs]);
-  return (
-    <div className="w-full p-4 bg-white rounded-md shadow-sm h-fit min-w-fit">
-      <Tabs defaultValue="live" className="flex flex-1 ">
-        <TabsList className="grid grid-cols-3 w-fit">
-          <TabsTrigger value="all">Tất cả</TabsTrigger>
-          <TabsTrigger value="live">Đang hoạt động</TabsTrigger>
-          <TabsTrigger value="hidden">Đã ẩn</TabsTrigger>
-        </TabsList>
-        <TabsContent value="all"></TabsContent>
-        <TabsContent value="live">
-          <ProductLive></ProductLive>
-        </TabsContent>
-        <TabsContent value="hidden"></TabsContent>
-      </Tabs>
-    </div>
-  );
+    router.replace('/products/all/1');
+  }, [router]);
+
+  return null;
 }
