755e9518cf7047b8e0be3f958c50c72ada7ca910
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-08 22:06:25 +0700

    Feat: Add product promotion new page

---
+++ FrontEnd/Staff/src/app/(main)/promotions/new/page.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useBreadcrumb } from '@/contexts/BreadcrumbContext';
+
+import api from '@/lib/axiosClient';
+import { useAuth } from '@/contexts/AuthContext';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+import Loader from '@/components/Loader';
+import ProductPromotionForm, { ProductPromotionFormType } from '../components/ProductPromotionForm';
+
+function mapFormToCreateDto(formData: ProductPromotionFormType, NV_id: string | null) {
+  return {
+    KM_ten: formData.name || '', // nếu optional thì check default ''
+    KM_id: formData.code,
+    KM_batDau: formData.from,
+    KM_ketThuc: formData.to,
+    NV_id: NV_id, // lấy từ người dùng hiện tại hoặc context
+    KM_chiTiet:
+      formData.detail?.map((item) => ({
+        KM_id: formData.code,
+        SP_id: item.productId,
+        CTKM_tyLe: item.isPercent,
+        CTKM_giaTri: item.value,
+        CTKM_tamNgung: item.isBlocked,
+      })) || [],
+  };
+}
+
+export default function ProductPromotionNew() {
+  const { setBreadcrumbs } = useBreadcrumb();
+  const { authData } = useAuth();
+  const router = useRouter();
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    setBreadcrumbs([
+      { label: 'Trang chủ', href: '/' },
+      { label: 'Giảm giá sản phẩm', href: '/promotions' },
+      { label: 'Thêm mới' },
+    ]);
+  }, [setBreadcrumbs]);
+
+  const onSubmit = (data: ProductPromotionFormType) => {
+    const apiData = mapFormToCreateDto(data, authData.userId);
+    console.log(apiData);
+    api
+      .post('/promotions', apiData)
+      .then(() => {
+        toast.success('Thêm mới thành công!');
+        router.back();
+      })
+      .catch((error) => {
+        if (error?.status === 400) {
+          toast.error('Thêm mới thất bại!');
+        } else {
+          toast.error('Đã xảy ra lỗi!');
+        }
+        console.error(error);
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
+  };
+
+  return (
+    <div className="relative w-full h-fit ">
+      <div className="w-full mx-auto h-fit min-w-fit max-w-4xl p-4">
+        <ProductPromotionForm onSubmit={onSubmit} />
+      </div>
+      {isSubmitting && <Loader />}
+    </div>
+  );
+}
