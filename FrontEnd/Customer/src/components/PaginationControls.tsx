fa2eee7b565d61a70159cd6f30d597e2081496d4
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-13 21:52:06 +0700

    Feat: Add pagination controller

---
+++ FrontEnd/Customer/src/components/PaginationControls.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationEllipsis,
+} from '@/components/ui/pagination';
+
+interface PaginationControlsProps {
+  pagination: number[];
+  currentPage: number;
+  totalPage: number;
+  onPageChange: (targetPage: number) => void;
+}
+
+export default function PaginationControls({
+  pagination,
+  currentPage,
+  totalPage,
+  onPageChange,
+}: Readonly<PaginationControlsProps>) {
+  const pages = pagination || [];
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        {/* Hiện trang 1 nếu thiếu */}
+        {pages.length > 0 && pages[0] !== 1 && (
+          <>
+            <PaginationItem>
+              <PaginationLink
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  onPageChange(1);
+                }}
+                isActive={currentPage === 1}
+              >
+                1
+              </PaginationLink>
+            </PaginationItem>
+
+            {/* Ellipsis nếu pages[0] > 2 (tức bỏ qua trang 2) */}
+            {pages[0] > 2 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+          </>
+        )}
+
+        {/* Render các trang hiện tại */}
+        {pages.map((page) => (
+          <PaginationItem key={page}>
+            <PaginationLink
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                if (page !== currentPage) onPageChange(page);
+              }}
+              isActive={page === currentPage}
+              aria-current={page === currentPage ? 'page' : undefined}
+            >
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        {/* Ellipsis nếu còn khoảng cách phía sau */}
+        {pages.length > 0 && pages[pages.length - 1] < totalPage - 1 && (
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        )}
+
+        {/* Hiện trang totalPage nếu bị thiếu */}
+        {pages.length > 0 && pages[pages.length - 1] !== totalPage && (
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                onPageChange(totalPage);
+              }}
+              isActive={currentPage === totalPage}
+            >
+              {totalPage}
+            </PaginationLink>
+          </PaginationItem>
+        )}
+      </PaginationContent>
+    </Pagination>
+  );
+}
