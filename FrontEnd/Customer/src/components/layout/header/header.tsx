7813eab9f6dc23f9bdb03437bc9c5d5faae53cc9
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-14 21:32:07 +0700

    Style: Update layput

--- FrontEnd/Customer/src/components/layout/header/header.tsx
+++ FrontEnd/Customer/src/components/layout/header/header.tsx
@@ -3,11 +3,10 @@ import CartButton from '@/components/layout/header/cartButton';
 import Link from 'next/link';
 import CategoryList from './categoryListButton';
 import SearchInput from './searchInput';
-
 export default function Header() {
   return (
     <header className="shadow sticky top-0 bg-white z-40">
-      <div className="container relative  mx-auto p-4 flex flex-wrap flex-row max-[320px]:flex-col  space-y-4 md:space-y-0">
+      <div className="container relative  mx-auto px-4 pt-4 flex flex-wrap flex-row max-[320px]:flex-col  space-y-4 md:space-y-0">
         {/* 1 */}
         <div className="order-1 w-1/2 max-[375px]:w-full md:order-1 md:w-auto font-bold text-2xl ">
           <Link href={'/'}>DẬT LẠC</Link>
