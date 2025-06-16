a4d6317b82238c8a7014fa2ea4959a43d6d2420b
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-16 22:34:30 +0700

    Refactoring: UPdate for matching component

--- FrontEnd/Customer/src/app/auth/components/login.tsx
+++ FrontEnd/Customer/src/app/auth/components/login.tsx
@@ -2,14 +2,14 @@
 
 import { useState } from 'react';
 import LoginForm from '@/app/auth/components/login-form';
-import ForgotPasswordForm from '@/app/auth/components/forgotPassword-form';
+import ChangePassword from '@/app/auth/components/changePassword';
 export default function Login() {
   const [showForgotPassword, setShowForgotPassword] = useState(false);
 
   return (
     <div className="max-w-lg w-full">
       {showForgotPassword ? (
-        <ForgotPasswordForm onBackToLogin={() => setShowForgotPassword(false)} />
+        <ChangePassword mode="forgot" onBackToLogin={() => setShowForgotPassword(false)} />
       ) : (
         <LoginForm onForgotPassword={() => setShowForgotPassword(true)} />
       )}
