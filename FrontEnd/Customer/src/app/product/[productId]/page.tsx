e4b09f8feb300300fc9283124c869b8f9dd3164a
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-16 22:31:52 +0700

    Refactoring: Update more value get from api

--- FrontEnd/Customer/src/app/product/[productId]/page.tsx
+++ FrontEnd/Customer/src/app/product/[productId]/page.tsx
@@ -7,8 +7,10 @@ import { useParams, useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { toast } from 'sonner';
 import ProductImageGallery from '../components/productImgs';
-import ProductBaseInfo from '../components/productBaseInf';
+import ProductInfo from '../components/productInf';
 import Loading from './loading';
+import Comments from '../components/comments';
+import { ProductList } from '@/components/product/productList';
 
 const mapProductToForm = (product: ProductDetailApiType): ProductDetailType => {
   const getCoverImageUrl = (images: ImageApi[]): string => {
@@ -23,9 +25,9 @@ const mapProductToForm = (product: ProductDetailApiType): ProductDetailType => {
   return {
     name: product.SP_ten,
     id: product.SP_id,
-    categories: product.SP_TL.map((tl: { id: number; ten: string }) => ({
-      id: tl.id,
-      name: tl.ten,
+    categories: product.SP_TL.map((tl: { TL_id: number; TL_ten: string }) => ({
+      id: tl.TL_id,
+      name: tl.TL_ten,
     })),
     status: product.SP_trangThai,
     summary: product.SP_tomTat,
@@ -43,9 +45,30 @@ const mapProductToForm = (product: ProductDetailApiType): ProductDetailType => {
     saled: product.SP_daBan,
     weight: product.SP_trongLuong,
     score: product.SP_diemDG,
-    salePrice: product.giaGiam,
+    salePrice: product.SP_giaGiam,
     coverImage: getCoverImageUrl(product.SP_anh),
     productImages: getProductImageUrls(product.SP_anh),
+    similar: product.SP_tuongTu.map((item) => {
+      const selePrice = item.SP_giaGiam === item.SP_giaBan ? undefined : item.SP_giaGiam;
+      const discountPercent = selePrice
+        ? Math.round(((item.SP_giaBan - item.SP_giaGiam) / item.SP_giaBan) * 100)
+        : undefined;
+
+      return {
+        id: item.SP_id,
+        name: item.SP_ten,
+        price: item.SP_giaBan,
+        cost: item.SP_giaNhap,
+        sold: item.SP_daBan,
+        stock: item.SP_tonKho,
+        image: item.SP_anh,
+        status: item.SP_trangThai,
+        score: item.SP_diemDG,
+        categories: item.TL_id,
+        selePrice: selePrice,
+        discountPercent: discountPercent,
+      };
+    }),
   };
 };
 
@@ -53,7 +76,6 @@ export default function ProductDetail() {
   const router = useRouter();
   const params = useParams();
   const id = params?.productId as string;
-
   const [data, setData] = useState<ProductDetailType>();
 
   const fetchProductDetail = async () => {
@@ -83,14 +105,34 @@ export default function ProductDetail() {
   if (!data) {
     return <Loading />;
   }
+
   return (
-    <div className="flex flex-wrap gap-4 h-full">
-      <div className="basis-0 flex-[5] min-w-86 h-124">
-        <ProductImageGallery coverImage={data.coverImage} productImages={data.productImages} />
+    <div className="space-y-4 ">
+      <div className="flex flex-col md:flex-row gap-4 relative">
+        {/* Cột trái: ảnh sản phẩm */}
+        <div className="basis-0 flex-[5] w-full">
+          <div className="sticky top-4  z-40">
+            <div className=" min-w-86 h-124 ">
+              <ProductImageGallery
+                coverImage={data.coverImage}
+                productImages={data.productImages}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Cột phải: nội dung sản phẩm */}
+        <div className="basis-0 flex-[7] w-full space-y-4">
+          <ProductInfo data={data} />
+        </div>
+      </div>
+      <div className="w-full p-4 rounded-md shadow bg-white">
+        <Comments id={data.id} score={data.score} />
       </div>
 
-      <div className="basis-0 flex-[7] min-w-72">
-        <ProductBaseInfo data={data} />
+      <div className="w-full p-4 rounded-md shadow bg-white">
+        <h2 className="font-semibold mb-4 text-lg">Sản phẩm tương tự</h2>
+        <ProductList displayType="carousel" products={data.similar} />
       </div>
     </div>
   );
