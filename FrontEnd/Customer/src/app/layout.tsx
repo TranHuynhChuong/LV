26ea7ed2d2123255951d189a60236b090836728a
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-18 19:52:08 +0700

    Style: Update postion of toast

--- FrontEnd/Customer/src/app/layout.tsx
+++ FrontEnd/Customer/src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           <Header />
           <main className="container mx-auto flex-1 flex flex-col w-full p-4">{children}</main>
           <Footer />
-          <Toaster richColors position="top-right" duration={2000} />
+          <Toaster richColors position="bottom-right" duration={2000} />
         </body>
       </html>
     </AuthProvider>
