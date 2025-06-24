8eec268362017ee27102b1b7a2a332e821679a11
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-24 15:18:05 +0700

    Refactoring: Optimize code

--- FrontEnd/Staff/src/app/(main)/accounts/page.tsx
+++ FrontEnd/Staff/src/app/(main)/accounts/page.tsx
@@ -1,14 +1,5 @@
-'use client';
+import { redirect } from 'next/navigation';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-export default function RedirectToFirstPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/accounts/staffs');
-  }, []);
-
-  return null;
+export default function Page() {
+  redirect('/accounts/staffs');
 }
