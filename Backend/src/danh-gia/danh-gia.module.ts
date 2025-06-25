5893df88050c1dda750073b893b9f71701c1e253
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-25 22:27:55 +0700

    Fix: Missing import

--- Backend/src/danh-gia/danh-gia.module.ts
+++ Backend/src/danh-gia/danh-gia.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DanhGiaService } from './danh-gia.service';
 import { DanhGiaController } from './danh-gia.controller';
+import { SanPhamModule } from 'src/san-pham/san-pham.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DanhGia, DanhGiaSchema } from './schemas/danh-gia.schema';
+import { NguoiDungModule } from 'src/nguoi-dung/nguoi-dung.module';
+import { DanhGiaRepository } from './repositories/danh-gia.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: DanhGia.name, schema: DanhGiaSchema }]),
+    SanPhamModule,
+    NguoiDungModule,
+  ],
   controllers: [DanhGiaController],
-  providers: [DanhGiaService],
+  providers: [DanhGiaService, DanhGiaRepository],
 })
 export class DanhGiaModule {}
