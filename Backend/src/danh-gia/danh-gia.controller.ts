fc52a0f6e6136bec1a0045476d6e2c25a2ef80eb
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-06-27 22:38:09 +0700

    Refactoring: Update api

--- Backend/src/danh-gia/danh-gia.controller.ts
+++ Backend/src/danh-gia/danh-gia.controller.ts
@@ -11,39 +11,39 @@ import { DanhGiaService } from './danh-gia.service';
 import { CreateDanhGiaDto } from './dto/create-danh-gia.dto';
 import { UpdateDanhGiaDto } from './dto/update-danh-gia.dto';
 
-@Controller('comments')
+@Controller('/api/reviews')
 export class DanhGiaController {
   constructor(private readonly DanhGiaService: DanhGiaService) {}
 
   @Post()
-  create(@Body() dto: CreateDanhGiaDto) {
+  create(@Body() dto: CreateDanhGiaDto[]) {
     return this.DanhGiaService.create(dto);
   }
 
-  @Get(':id')
+  @Get('/:id')
   getById(@Param('id') id: string) {
     return this.DanhGiaService.getById(id);
   }
 
-  @Get()
-  getAllByProduct(
-    @Query('productId') spId: string,
+  @Get('/product/:productId')
+  getAllOfProduct(
+    @Param('productId') spId: string,
     @Query('page') page: string = '1',
     @Query('limit') limit: string = '24'
   ) {
-    return this.DanhGiaService.getAllByProduct(
+    return this.DanhGiaService.getAllOfProduct(
       +spId,
       parseInt(page),
       parseInt(limit)
     );
   }
 
-  @Patch(':id/hide')
+  @Patch('/:id/hide')
   hide(@Param('id') id: string, @Body() dto: UpdateDanhGiaDto) {
     return this.DanhGiaService.hide(id, dto);
   }
 
-  @Patch(':id/show')
+  @Patch('/:id/show')
   show(@Param('id') id: string, @Body() dto: UpdateDanhGiaDto) {
     return this.DanhGiaService.show(id, dto);
   }
