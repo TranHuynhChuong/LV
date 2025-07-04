3829844c08b4523e2cdee168d4fb2da3af9df238
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-07-04 15:36:13 +0700

    Refactoring: Update import

--- Backend/src/Util/util.module.ts
+++ Backend/src/Util/util.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { EmailService } from './email.service';
 import { CloudinaryService } from './cloudinary.service';
 import { TransformService } from './transform.service';
+import { ExportService } from './export';
 @Global()
 @Module({
   imports: [ConfigModule],
-  providers: [EmailService, CloudinaryService, TransformService],
-  exports: [EmailService, CloudinaryService, TransformService],
+  providers: [EmailService, CloudinaryService, TransformService, ExportService],
+  exports: [EmailService, CloudinaryService, TransformService, ExportService],
 })
 export class UtilModule {}
