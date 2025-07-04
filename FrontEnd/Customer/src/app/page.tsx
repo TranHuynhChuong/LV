49084f621f8e6e3127f9c958ad590c49e5c2f42f
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-07-04 15:38:40 +0700

    Feat: Update home page with 3 reconmmend product: most rating, most selling, latest

--- FrontEnd/Customer/src/app/page.tsx
+++ FrontEnd/Customer/src/app/page.tsx
@@ -1,3 +1,39 @@
-export default function Home() {
-  return <div className="w-full h-full"></div>;
+import { Suspense } from 'react';
+import api from '@/lib/axios';
+import { ProductSortType, mapProductOverviewListFromDto } from '@/models/products';
+import HomeComponent from '@/components/home/homeComponent';
+
+export default async function HomePage() {
+  const pageSize = 8;
+
+  const fetchProducts = async (sort: ProductSortType) => {
+    try {
+      const res = await api.get('/products/search', {
+        params: {
+          keyword: '',
+          categoryId: '',
+          page: 1,
+          sortType: sort,
+          filterType: 'show-all',
+          limit: pageSize,
+        },
+      });
+      return mapProductOverviewListFromDto(res.data.data);
+    } catch (error) {
+      console.error(`Error fetching products sorted by ${sort}:`, error);
+      return [];
+    }
+  };
+
+  const [mostRated, latest, bestSelling] = await Promise.all([
+    fetchProducts(ProductSortType.MostRating),
+    fetchProducts(ProductSortType.Latest),
+    fetchProducts(ProductSortType.BestSelling),
+  ]);
+
+  return (
+    <Suspense fallback={<></>}>
+      <HomeComponent mostRated={mostRated} latest={latest} bestSelling={bestSelling} />
+    </Suspense>
+  );
 }
