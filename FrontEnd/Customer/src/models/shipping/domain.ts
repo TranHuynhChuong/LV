940d0a761f520389119bed45c1ca7700883376ce
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-07-04 16:09:02 +0700

    Feat: Add shipping fee model

---
+++ FrontEnd/Customer/src/models/shipping/domain.ts
@@ -0,0 +1,9 @@
+export type ShippingFee = {
+  id?: number;
+  provinceId?: number;
+  province?: string;
+  fee?: number;
+  weight?: number;
+  surcharge?: number;
+  surchargeUnit?: number;
+};
---
+++ FrontEnd/Customer/src/models/shipping/dto.ts
@@ -0,0 +1,9 @@
+export type ShippingFeeDto = {
+  PVC_id?: number;
+  PVC_phi: number;
+  PVC_ntl: number;
+  PVC_phuPhi: number;
+  PVC_dvpp: number;
+  T_id: number;
+  NV_id?: string;
+};
