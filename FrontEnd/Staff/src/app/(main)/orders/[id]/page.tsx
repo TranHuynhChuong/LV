476fe1e77ee175036e4a4dd36fff7bd6fb725ccb
TranHuynhChuong <tranhuynhchuong00@gmail.com>
2025-07-31 17:26:00 +0700

    Style: Fix layout

--- FrontEnd/Staff/src/app/(main)/orders/[id]/page.tsx
+++ FrontEnd/Staff/src/app/(main)/orders/[id]/page.tsx
@@ -3,8 +3,10 @@ import OrderDetail from '@/components/orders/order-detail';
 export default function Page() {
   return (
     <div className="p-4">
+      <div className="relative w-full mx-auto">
       <OrderDetail />
-      <div className="relative w-full mx-auto"></div>
+
+      </div>
     </div>
   );
 }
